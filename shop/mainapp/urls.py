da7c0701e942018ec3441324be8a15e905667d63
Ivan Filippov <i-va-n228@yandex.ru>
2021-02-12 16:10:15 +0300

    Count of product in cart and messages

--- shop/mainapp/urls.py
+++ shop/mainapp/urls.py
@@ -6,7 +6,8 @@ from .views import (
     CategoryDetailView,
     CartView,
     AddToCartView,
-    DeleteFromCartView
+    DeleteFromCartView,
+    ChangeQTYView,
 )
 
 urlpatterns = [
@@ -16,5 +17,6 @@ urlpatterns = [
     path('cart/', CartView.as_view(), name='cart'),
     path('add-to-cart/<str:ct_model>/<str:slug>', AddToCartView.as_view(), name='add_to_cart'),
     path('remove-from-cart/<str:ct_model>/<str:slug>', DeleteFromCartView.as_view(), name='delete_from_cart'),
+    path('change-qty/<str:ct_model>/<str:slug>', ChangeQTYView.as_view(), name='change_qty'),
 ]
 
